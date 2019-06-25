467a4f1a43c2945c358414c3d65ffd5eff2ff736
James Kinyua <jameskinyua590@gmail.com>
2019-06-25 21:53:27 +0300

    refactor: split webpack into common, prod and dev config files1

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -18,19 +20,15 @@ module.exports = {
         }
       },
       {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' }
-        ],
-      },
-      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           { loader: 'file-loader' }
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({ title: 'My killer app' })
+  ]
 };
